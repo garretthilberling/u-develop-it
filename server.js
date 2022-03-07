56a2d87026d530b94fc89560e463da26ec48f4f5
garretthilberling <ghilberling@knights.ucf.edu>
2022-03-06 23:23:03 -0500

    got sql database to work with node and express

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
+const mysql = require('mysql2');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'qcB$s^d(j*95,:L+Q!@ffZc',
+        database: 'election'
+    },
+    console.log('Connected to the election database')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
